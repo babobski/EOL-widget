0cae5b65d013ca55be66945dc304bf9a57830d2d
babobski <info@babobski.nl>
2015-10-24 22:06:09 +0200

    updated plugin so you can change the current file EOL

--- content/overlay.js
+++ content/overlay.js
@@ -11,20 +11,31 @@ if (typeof(extensions.EOL_widget) === 'undefined') extensions.EOL_widget = {
     var $       = require("ko/dom");
 	var notify	= require("notify/notify");
     var eol     = ['CRLF', 'CR', 'LF'];
-
-    window.removeEventListener('current_view_changed', extensions.addEOL);
-    window.removeEventListener('focus', extensions.addEOL);
-    window.removeEventListener('view_opened', extensions.checkEOL);
+ 
+    window.removeEventListener('current_view_changed', this.addEOL);
+    window.removeEventListener('focus', this.addEOL);
+    window.removeEventListener('view_opened', this.checkEOL);
 
     $("#statusbar-eol").remove();
-    $("#statusbar-encoding").before($("<statusbarpanel id='statusbar-eol'/>"));
-
-    var panel   = $('#statusbar-eol');
-    panel.append("<label class='statusbarpanel-text'/>");
-    var label = $('#statusbar-eol > label');
+    $("#statusbar-encoding").before($("<statusbarpanel tooltiptext='File Line Ending' id='statusbar-eol'/>"));
+	var menu 		= $('<menupopup id="statusbar-eol-menupopup"/>');
+	var itemCRLF 	= $('<menuitem label="CRLF" oncommand="extensions.EOL_widget.setEol(\'CRLF\');" />');
+	var itemLF		= $('<menuitem label="LF" oncommand="extensions.EOL_widget.setEol(\'LF\');" />');
+	var item		= $('<menuitem label="CR" oncommand="extensions.EOL_widget.setEol(\'CR\');" />');
+	menu.append(itemCRLF);
+	menu.append(itemLF); 
+	menu.append(item);
+	
+    var panel   	= $('#statusbar-eol');
+    panel.append('<toolbarbutton class="statusbar-label" id="statusbar-eol-label" flex="1" orient="horizontal" type="menu" persist="buttonstyle" buttonstyle="text" label="" />');
+    var button = $('#statusbar-eol > #statusbar-eol-label');
+	
+	button.append(menu); 
 
     var timer, timerRunning;
-    extensions.addEOL = function(){
+    this.addEOL = function(){ 
+		var koDoc 	= require('ko/views').current().koDoc;
+		var prefs 	= require('ko/views').current().prefs;
         var view = ko.views.manager.currentView;
 		if (view == null){
             return;
@@ -34,11 +45,13 @@ if (typeof(extensions.EOL_widget) === 'undefined') extensions.EOL_widget = {
             return;
         }
         
-        label.attr('value', eol[scimoz.eOLMode]);
+        button.attr('label', prefs.getString('endOfLine'));
     };
     
-    extensions.checkEOL = function() {
+    this.checkEOL = function() {
        var view = ko.views.manager.currentView;
+	   var koDoc 	= require('ko/views').current().koDoc;
+		var prefs 	= require('ko/views').current().prefs;
 		if (view == null){
             return;
         }
@@ -47,7 +60,11 @@ if (typeof(extensions.EOL_widget) === 'undefined') extensions.EOL_widget = {
             return;
         }
 		
-        var fileEOL 	= eol[scimoz.eOLMode];
+		var prefs = require('ko/views').current().prefs; 
+		if ( ! prefs) return;
+		
+		
+        var fileEOL 	= prefs.getString('endOfLine');
         var globalEOL 	= ko.prefs.getString('endOfLine');
         if (fileEOL !== globalEOL) {
             notify.send(
@@ -56,9 +73,30 @@ if (typeof(extensions.EOL_widget) === 'undefined') extensions.EOL_widget = {
             );
         }
     }
+	
+	this.setEol = function(value) {
+		var koDoc 		= require('ko/views').current().koDoc;
+		var prefs 		= require('ko/views').current().prefs;
+		var pref 		= Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefService).getBranch("extensions.EOL_widget.");
+		var override	= pref.getBoolPref('preserveExisting') ? false : true;
+		if ( ! prefs) return;
+		
+		prefs.setString('endOfLine', value);
 
-    window.addEventListener('current_view_changed', extensions.addEOL);
-    window.addEventListener('focus', extensions.addEOL);
-    window.addEventListener('view_opened', extensions.checkEOL);
+		eol = koDoc.EOL_LF; 
+		if (value == "CR")
+			eol = koDoc.EOL_CR;
+		else if (value == "CRLF")
+			eol = koDoc.EOL_CRLF;
+		koDoc.new_line_endings = eol;
+		
+		if (override) koDoc.existing_line_endings = eol;
+	}
+
+
+    window.addEventListener('current_view_changed', this.addEOL);
+    window.addEventListener('focus', this.addEOL);
+    window.addEventListener('view_opened', this.checkEOL);
 
 }).apply(extensions.EOL_widget);
